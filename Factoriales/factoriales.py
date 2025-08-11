5d0626ff44f3c4462070e5332766ab2d574d294b
Victor <cordobavictorml@gmail.com>
2025-08-11 18:49:25 -0500

    Factoriales Hecho

--- Factoriales/factoriales.py
+++ Factoriales/factoriales.py
@@ -1,1 +1,5 @@
-print("Hola github")
+num=int(input("Digite un numero para ver su factorial: "))
+factorial=1
+for i in range(1,num+1):
+    factorial=factorial*i
+print(f"El factorial de '{num}' es: {factorial}")
