5985879465e19968b5d9771d401acae0a497de9d
Victor <cordobavictorml@gmail.com>
2025-08-13 15:01:55 -0500

    Commit 4 calculadora

--- TablaMulti/tablamulti.py
+++
@@ -1,8 +0,0 @@
-num=int(input("Digite un numero para ver su tabla de mulpiplicar: "))
-for i in range(10):
-    print(f"{num} X {(i+1)} = {(num*(i+1))}")
-
-""""
-for i in range(1,11):
-    print(f"{num} X {i} = {(num*i)}")
-"""
