e06a7a6100f46c89afdf3e225886ac67a5c71928
Victor <cordobavictorml@gmail.com>
2025-08-25 14:06:39 -0500

    Agregados cambios locales pendientes

--- Factorial/factorial.py
+++ Factorial/factorial.py
@@ -2,4 +2,4 @@ num=int(input("Digite un numero para ver su factorial: "))
 factorial=1
 for i in range(1,num+1):
     factorial=factorial*i
-print(f"El factorial de '{num}' es: {factorial}")
+print(f"El factorial de '{num}' es el: {factorial}")
---
+++ MenuTxt/menuTexto.py
@@ -0,0 +1,47 @@
+while True:
+    texto=str(input("\nIngrese el texto que desea convertir: "))
+
+    print("\n==Mostrar Menu==")
+    print("1.Primera Letra en Mayuscula")
+    print("2.Primeras Letras en Mayuscula")
+    print("3.Todo en Minuscula")
+    print("4.Todo en Mayuscula")
+    print("5.Salir")
+
+    # Solicitar al usuario que elija u8na opcion del menu
+    op=input("Selecciones una de las opciones (1-5): ")
+
+    # ====OPeraciones====
+
+    #OpcioN 1: Primera Letra en Mayuscula
+    if op== '1':
+        resultado= texto.capitalize()
+        print(f"Tu nuevo texto: {resultado}")
+
+    #OpcioN 2: Primeras letras en Mayusculas
+    elif op == '2':
+        resultado= texto.title()
+        print(f"Tu nuevo texto: {resultado}")
+
+
+    #OpcioN 3: Todo Minuscula
+    elif op == '3':
+        resultado= texto.lower()
+        print(f"Tu nuevo texto: {resultado}")
+
+    #OpcioN 4: Todo Mayuscula
+    elif op == '4':
+        resultado= texto.upper()
+        print(f"Tu nuevo texto: {resultado}")
+
+    #OpcioN 5: Salir
+    elif op == '5':
+        print("Hasta luego....")
+        break
+    
+    #Casp por defecto: OP invalida
+    else:
+        print("\n=========\nOpcion invalida. Intenta denuevo de 1 al 5")
+
+    #Se agrega una pausa para que el usuario pueda ver el resuldado antes de continuar
+        input("\nPresione Enter para continuar... ")
